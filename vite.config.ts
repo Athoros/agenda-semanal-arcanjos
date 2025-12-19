1824fbc991692c3296f6137e6f7a34a77695494b
Athoros <36735504+Athoros@users.noreply.github.com>
2025-12-19 14:46:02 -0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa'; <--- Comentado por enquanto
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      // ESTA É A CORREÇÃO: Ensinamos o Vite onde encontrar o arquivo fantasma
-      'react/jsx-runtime': 'react/jsx-runtime.js',
-    },
-  },
   plugins: [
-    react(), // Voltamos ao padrão (sem 'classic')
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
-      manifest: {
-        name: 'Agenda Semanal Arcanjos',
-        short_name: 'Agenda Arcanjos',
-        description: 'Agenda para controle de presença no escritório.',
-        theme_color: '#1E2024',
-        background_color: '#1E2024',
-        display: 'standalone',
-        orientation: 'portrait',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
+    react(), 
+    // VitePWA({...}) <--- Removemos o bloco do PWA inteiro por enquanto
   ],
 });
