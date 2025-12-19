c8e5a759b4f9c1ac241afc5659b24e61295c133f
Athoros <36735504+Athoros@users.noreply.github.com>
2025-12-19 15:13:53 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,34 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import { VitePWA } from 'vite-plugin-pwa'; <--- Comentado por enquanto
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
-    react(), 
-    // VitePWA({...}) <--- Removemos o bloco do PWA inteiro por enquanto
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Agenda Semanal Arcanjos',
+        short_name: 'Agenda Arcanjos',
+        description: 'Agenda para controle de presença no escritório.',
+        theme_color: '#1E2024',
+        background_color: '#1E2024',
+        display: 'standalone',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
   ],
 });
